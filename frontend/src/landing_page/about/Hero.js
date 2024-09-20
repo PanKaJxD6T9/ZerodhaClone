c1ac179644922f046126a5a96af1a5ff95debea4
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-09-20 16:44:38 +0530

    Completed About Page

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <div className='container mb-5'>
+        <div className='row border-bottom mb-5'>
+          <div style={{width:'65%', marginTop:'170px', marginBottom:'100px'}} className='col-12 text-center mx-auto'>
+            <h1 style={{fontSize:'35px'}}>We pioneered the discount broking model in India.
+            Now, we are breaking ground with our technology.</h1>
+          </div>
+        </div>
+        <div style={{marginTop:'100px', marginBottom:'100px'}} className='row justify-content-center'>
+          <div style={{width:'35%'}} className='col-6'>
+            <p>We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.</p>
+            <p>Today, our disruptive pricing models and in-house technology have made us the biggest stock broker in India.</p>
+            <p>Over 1+ Crore clients place millions of orders every day through our powerful ecosystem of investment platforms, contributing over 15% of all Indian retail trading volumes.</p>
+          </div>
+          <div style={{width:'35%'}} className='col-6'>
+            <p>In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.</p>
+            <p>Rainmatter, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.</p>
+            <p>And yet, we are always up to something new every day. Catch up on the latest updates on our blog or see what the media is saying about us.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="container mb-5">
+      <h1 className="text-center mb-5">People</h1>
+      <div style={{marginTop:'100px'}} className="row">
+        <div style={{paddingRight:'0px'}} className="col-6 d-flex flex-column justify-content-end align-items-center">
+          <img style={{width:'50%', borderRadius:'50%'}} src="assets/nithinKamath.jpg" alt="team1" />
+          <p style={{fontSize:'20px'}} className="mt-4">Nithin Kamath</p>
+          <p style={{fontSize:'15px'}} className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-6">
+          <p style={{fontSize:'18px'}} className="mb-4">Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+          <p style={{fontSize:'18px'}} className="mb-4">He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+          <p style={{fontSize:'18px'}} className="mb-4">Playing basketball is his zen.</p>
+          <p style={{fontSize:'18px'}} className="mb-4">Connect on Homepage / TradingQnA / Twitter</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Team
+export default Team;
