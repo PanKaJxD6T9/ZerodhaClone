b34adef9204b08a0078c7b3fa2d6212a6d31f684
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-09-22 10:12:46 +0530

    Support Section Done

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -2,7 +2,221 @@ import React from 'react'
 
 const CreateTicket = () => {
   return (
-    <div>CreateTicket</div>
+    <div style={{ marginTop: '175px' }} className="container">
+      <h4 style={{paddingLeft: '90px'}} className="mb-5 text-muted">To create a ticket, select a relevant topic</h4>
+      <div style={{paddingLeft: '90px'}} className="row mb-5">
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-solid fa-circle-plus px-2"></i>Account Opening
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Getting started
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Online
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Offline
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Charges
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Company, Partnership and HUF
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Non Resident Indian (NRI)
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-regular fa-user px-2"></i> Your Zerodha Account
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Login credentials
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Your Profile
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Account modification and segment addition
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                CMR & DP ID
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Nomination
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Transfer and conversion of shares
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-solid fa-arrow-trend-up px-2"></i> Trading and
+            Markets
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Trading FAQs
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Kite
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Margins
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Product and order types
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Corporate actions
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Kite features
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div style={{marginBottom: '90px', paddingLeft: '100px'}} className="row">
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-solid fa-credit-card px-2"></i> Funds
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Fund withdrawal
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Adding funds
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Adding bank accounts
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                eMandates
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-solid fa-copyright px-2"></i> Console
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                IPO
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Portfolio
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Funds statement
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Profile
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Reports
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Referral program
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h5 className="mb-4">
+            <i className="fa-brands fa-nfc-directional px-2"></i> Coin
+          </h5>
+          <ul style={{ fontSize: '14px' }} className="list-unstyled px-5">
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Understanding mutual funds and Coin
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Coin app
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Coin web
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                Transactions and reports
+              </a>
+            </li>
+            <li className="mb-3">
+              <a style={{ textDecoration: 'none', color: '#387ED1' }} href="#">
+                National Pension Scheme (NPS)
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,7 +2,111 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div style={{ backgroundColor: '#387ED1' }}>
+      <div
+        style={{ marginTop: '75px', paddingLeft: '65px' }}
+        className="container py-5"
+      >
+        <div className="d-flex justify-content-between text-white mb-5">
+          <h5>Support Portal</h5>
+          <h5>
+            <a
+              style={{ color: 'white', textUnderlineOffset: '0.4em' }}
+              href="#"
+            >
+              Track Tickets
+            </a>
+          </h5>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <h4 style={{ color: 'white' }} className="mb-4">
+              Search for an answer or browse help topics to create a ticket
+            </h4>
+            <input
+              style={{
+                width: '100%',
+                height: '60px',
+                padding: '25px',
+                outline: 'none',
+              }}
+              className="mb-3"
+              type="text"
+              placeholder="Eg: how do i activate F&O"
+            />
+            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+              <a
+                style={{
+                  color: '#ffffff',
+                  fontSize: '18px',
+                  textUnderlineOffset: '0.4em',
+                }}
+                href="#"
+              >
+                Track account opening
+              </a>
+              <a
+                style={{
+                  color: '#ffffff',
+                  fontSize: '18px',
+                  textUnderlineOffset: '0.4em',
+                }}
+                href="#"
+              >
+                {' '}
+                Track segment activation
+              </a>
+              <a
+                style={{
+                  color: '#ffffff',
+                  fontSize: '18px',
+                  textUnderlineOffset: '0.4em',
+                }}
+                href="#"
+              >
+                {' '}
+                Intraday margins
+              </a>
+              <a
+                style={{
+                  color: '#ffffff',
+                  fontSize: '18px',
+                  textUnderlineOffset: '0.4em',
+                }}
+                href="#"
+              >
+                Kite user manual
+              </a>
+            </div>
+          </div>
+          <div className="col-1"></div>
+          <div
+            style={{ flexDirection: 'column', color: 'white' }}
+            className="col-5 d-flex justify-content-center px-5"
+          >
+            <h4 className="mb-4">Featured</h4>
+            <ol>
+              <li className="mb-3">
+                <a
+                  style={{ color: 'white', textUnderlineOffset: '0.4em' }}
+                  href="#"
+                >
+                  Surveillance measure on scrips - September 2024
+                </a>
+              </li>
+              <li>
+                <a
+                  style={{ color: 'white', textUnderlineOffset: '0.4em' }}
+                  href="#"
+                >
+                  Rights Entitlements listing in September 2024
+                </a>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
